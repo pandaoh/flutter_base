3561c41f17a4fc5e79faa19bc4e44ccbe400d0ca
doubleam <admin@biugle.cn>
2023-11-15 18:12:59 +0800

    Fix: dio base utl bug.

--- sbt_rfid_pda/lib/config/helper.dart
+++ sbt_rfid_pda/lib/config/helper.dart
@@ -2,16 +2,16 @@
  * @Author: HxB
  * @Date: 2023-11-03 10:05:10
  * @LastEditors: DoubleAm
- * @LastEditTime: 2023-11-03 10:05:24
+ * @LastEditTime: 2023-11-15 18:11:39
  * @Description: 全局通用方法
- * @FilePath: \rfid_pda\lib\config\helper.dart
+ * @FilePath: \sbt_rfid_pda\sbt_rfid_pda\lib\config\helper.dart
  */
 class Helper {
   static demo() async {
     // do something
   }
 
-  static getApiUrl() async {
+  static String getApiUrl() {
     return 'http://192.168.110.13';
   }
 }
